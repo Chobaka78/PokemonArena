74d48924c2831b8c45b5b9ea9621d0e23f0bc09d
Chobaka78 <34846941+Chobaka78@users.noreply.github.com>
2018-12-09 00:52:23 -0500

    Update 5
    fixed battle
    (does crash after a while)

--- PokemonArena.java
+++ PokemonArena.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class PokemonArena{
-	public static ArrayList<Pokemon> PokemonArray = new ArrayList<Pokemon>(); // arraylist that contains all the pokemons 
-	public static ArrayList<Pokemon> pokemonTeam = new ArrayList<Pokemon>(); // Arraylist that contains all the pokemons the user choses as their team
+	private static ArrayList<Pokemon> PokemonArray = new ArrayList<Pokemon>(); // arraylist that contains all the pokemons 
+	private static ArrayList<Pokemon> pokemonTeam = new ArrayList<Pokemon>(); // Arraylist that contains all the pokemons the user choses as their team
 	public static Scanner kb = new Scanner(System.in);
 	public static Pokemon user; // making an user object
 	public static Pokemon enemy; // making an enemy object
@@ -11,8 +11,25 @@ public class PokemonArena{
 	public static int attackChoice;
 	public static int epick;
 	public static boolean disable;
+	public static boolean turn, stunned, play;
+
 	public static void main(String [] args) throws IOException{ // when loading the file it may throw and exception and this will catch it
 		
+		startup();
+		load();
+		if(play == true){
+		pickpokemon();
+		chooseUser(); // lets the user choose their pokemon
+		chooseEnemy();
+			while(true){
+				randomTurn(); // rnandomizes the turn
+				playgame(); // runs the play game method
+			}
+		}
+
+	}
+
+	public static void startup(){
 		int answer = 0;
 		System.out.println("WELCOME TO POKEMON ARENA!!!");
 		System.out.println("\n" + "Please enter your name: ");
@@ -21,23 +38,11 @@ public class PokemonArena{
 		System.out.println("1.YES" + "\n" + "2.NO");
 		answer = kb.nextInt();
 
-		
 		if(answer == 1){ // if the user enters yes then begin the game
 			System.out.println("Great, let's begin, " + name + "\n");
-			load();
-			pickpokemon();
-			System.out.println("Are you ready to battle? " + "\n" + "1. YES" + "\n" + "2. NO");
-			answer = kb.nextInt();
-			if(answer == 1){ // if user enters yes then start the battle
-				chooseUser(); // lets the user choose their pokemon
-				chooseEnemy();
-				randomTurn(); // rnandomizes the turn
-				//playgame(); // runs the play game method
-			}
-			else{ // if the user picks no as their answer then end the game 
-				System.out.println("Good bye"); 
-			}
+			play = true;
 		}
+
 		else if(answer == 2){
 			System.out.println("Okay then, goodbye!");
 		}
@@ -74,7 +79,7 @@ public class PokemonArena{
 		
 		System.out.println("Your team is" + pokemonTeam);
 	}
-	public static void chooseUser(){
+	public static Pokemon chooseUser(){
 		if(pokemonTeam.size() > 0){
 			System.out.println("Choose a pokemon to fight with: ");
 			for (int i = 0; i < pokemonTeam.size(); i++){
@@ -84,7 +89,7 @@ public class PokemonArena{
 		    	int choice = kb.nextInt();
 		    	if (choice > 0 && choice < pokemonTeam.size() + 1){
 		    		user = pokemonTeam.get(choice-1); // takes chosen Pokemon from remaining Pokemon to choose from, and it becomes the user
-		    		System.out.println(user.name + "! I choose you!" + "\n");
+		    		System.out.println(user.name + "! I choose you!");
 		    		break;
 		    	}
 		    	else{
@@ -92,23 +97,24 @@ public class PokemonArena{
 		    	}
 	    	}
 		}
+		return user;
 	}
 
 	public static void chooseEnemy(){
 		if(PokemonArray.size() > 0){
 			epick = (int)(Math.random()* PokemonArray.size()); // for picking a random pokemon for the enemy
 			enemy = PokemonArray.get(epick);
+			System.out.println("It's " + user.name + " vs " + enemy.name);
 		}
 	}
 
 	public static void randomTurn(){ // randomizes the turn
 		int num = (int)(Math.random()* 2);
 		if(num == 0){
-			user_battle(user, enemy);
+			turn = true;
 		}
 		if(num == 1){
-			System.out.println("\n" + "--------BATTLE--------" + "\n");
-			enemy_battle(enemy, user);
+			turn = false;
 		}
 	}
 	
@@ -124,147 +130,135 @@ public class PokemonArena{
 		}
 	}
 	
-	// public static void playgame(){
-	// 	disable = false;
-	// 	int choice = 0;
-	// 	int energycost = user.leastEnergy;
-	// 	chooseEnemy();
-	// 	System.out.println("It's " + user.name + " vs " + enemy.name + "!!!");
-	// 	if(turn == true){
-	// 		System.out.println("\n" + "What would you like to do with this pokemon?" + "\n");
-	// 		System.out.println("1.Attack    2.Pass    3.Retreat");
-	// 		choice = kb.nextInt();
-	// 		if(choice == 1 && user.energy < energycost){
-	// 			System.out.println("You do not have enough energy for this attack");
-	// 			choice = 2;
-	// 		}
-	// 		else if(choice == 1 && user.energy > energycost){
-	// 			System.out.println("\n" + user.leastEnergy + "\n");
-	// 			user_battle();	
-	// 		}
-	// 		else if(choice == 2){
-	// 			System.out.println("You have passed your turn");
-	// 			turn = false;
-	// 		}
-	// 		else if(choice == 3){
-	// 			System.out.println("You have chosen to retreat");
-	// 			pokemonTeam.remove(user);
-	// 			chooseUser();
-	// 			user_battle();
-	// 			turn = false;
-	// 		}
-
-	// 	}
-	// 	if(turn == false){
-	// 		enemy_battle();
-	// 	}
-	// }
-
-	public static void user_battle(Pokemon user, Pokemon enemy){
-		disable = false;
-		int round = 0;
-		int answer = 0;
-		int choice = 0;
-		// asking the user what they would like to do with the pokemon
-		System.out.println("It's " + user.name + " vs " + enemy.name);
-		System.out.println("\n" + "What would you like to do with this pokemon?" + "\n");
-		System.out.println("1.Attack    2.Pass    3.Retreat");
-		choice = kb.nextInt();
-		if(choice ==1 && user.energy < user.leastEnergy){ 
-			System.out.println("You cant afford this attack");
-			choice = 2; //pass
-		}
-		// if the choice is attack and they have sufficient energy
-		else if(choice == 1 && user.energy > user.leastEnergy){
-			//update on pokemon info
-			System.out.println("\n" + user.name + " has " + user.energy + " energy" + "\n");
-			System.out.println("You have " + user.nums_attack + " attacks to choose from:");
-			for (int i = 0; i < user.nums_attack; i++){
-	    		System.out.println(i+1 + ". " + user.attack.get(i).name + ", Energy: " + user.attack.get(i).energy + ", Damage: " + user.attack.get(i).damage + ", Effect: " + user.attack.get(i).effect);
-			}
-
-			while(true){
-				attackChoice = kb.nextInt();
-				if(enemy.hp > 0){ 
-				enemy.hp -= user.attack.get(attackChoice - 1).damage;
-				System.out.println("\n" + "--------BATTLE--------" + "\n");
-				System.out.println(user.name + " attacks " + enemy.name + " with " + user.attack.get(attackChoice - 1).name + "\n");
-				System.out.println(enemy + " has " + (enemy.hp < 0 ? "0" : enemy.hp) + " hp left"); // a terminary opperator
-				user.energy -= user.attack.get(attackChoice - 1).energy;
-				// if pokemon faitnts then end the battle
-					if(enemy.hp <= 0){
-						System.out.println(enemy.name + " fainted!");
-						PokemonArray.remove(enemy);
-						epick = (int)(Math.random()* PokemonArray.size());
-						enemy = PokemonArray.get(epick);
-						System.out.println("\n" + "------END BATTLE------" + "\n");
-						System.out.println("would you like to change your pokemon?" + "\n" + "1.YES" + "\n" + "2.NO");
-						answer = kb.nextInt();
-						if(answer == 1){
-							chooseUser();
-						}
-						else if (answer == 2){
-							System.out.println("It's " + user.name + " vs " + enemy.name + "!!!");
-							System.out.println("\n" + "--------BATTLE--------" + "\n");
-						}
-						break;
-					}
-					else{
-						break;
-					}
-				}
-			}
-			enemy_battle(enemy, user);
-		}
-		// if the choice is to pass
-		if(choice == 2){
-			System.out.println("You have passed your turn" + "\n");
-			enemy_battle(enemy, user);
-		}
-
-		// if the choice is to retreat
-		if(choice == 3){
-			System.out.println("You have chosen to retreat" + "\n");
-			pokemonTeam.remove(user);
-			chooseUser();
-			enemy_battle(enemy, user);
-		}
-
-	}
-
-	public static void enemy_battle(Pokemon enemy, Pokemon user){
-		int eattack = (int)(Math.random()* enemy.attack.size());
-		int e_attpick = (int)(Math.random()* 1);
-		boolean disable = false;
-
+	public static void playgame(){
 		while(true){
-			if(user.hp > 0){
-				System.out.println(enemy.name + " has " + enemy.energy + " energy" + "\n");
-				user.hp -= enemy.attack.get(eattack).damage;
-				System.out.println(enemy.name + " attacks " + user.name + " with " + enemy.attack.get(eattack).name + "\n");
-				System.out.println(user + " has " + (user.hp < 0 ? "0" : user.hp) + " hp left"); // a terminary opperator
-				if(user.hp <= 0){
-					System.out.println("Oh no! " + user.name + " fainted");
-					pokemonTeam.remove(user);
-					chooseUser();
-					System.out.println("It's " + user.name + " vs " + enemy.name + "!!!");
-					break;
-				}
-				else{
-					break;
-				}
-			}
-		}
-		System.out.println("\n" + "------END BATTLE------" + "\n");
-		for(int i = 0; i < pokemonTeam.size(); i++){
-			if(pokemonTeam.get(i).energy < 50){
-				pokemonTeam.get(i).energy += 10;
-				if(pokemonTeam.get(i).energy > 50){
+			battle();
+			if(user.hp == 0){
+				System.out.println("Oh no! " + user.name + "has fainted");
+				System.out.println("\n" + "---END BATTLE---" + "\n");
+				pokemonTeam.remove(user);
+				chooseUser();
+				for(int i = 0; i < pokemonTeam.size(); i++){
 					pokemonTeam.get(i).energy = 50;
 				}
+				randomTurn();
 			}
+			if(enemy.hp == 0){
+				System.out.println(enemy.name + " has no hp left " + enemy.name + " has fainted");
+				System.out.println("\n" + "---END BATTLE---" + "\n");
+				PokemonArray.remove(enemy);
+				if(pokemonTeam.size() > 1){
+					System.out.println("Would you like to change your pokemon" + "\n" + "1.YES" + "\n" + "2.NO");
+					while(true){
+						int a = kb.nextInt();
+						if(a == 1){
+							chooseUser();
+							break;
+						}
+						else if(a == 2){
+							break;
+						}
+						else{
+							System.out.println("Invalid choice please pick a valid choice");
+						}
+					}
+				}
+				chooseEnemy();
+				//();
+			}
+			if(pokemonTeam.size() == 0){
+				System.out.println("You couldnt deafeat all the pokemons, there were " + PokemonArray.size() + " pokemons remaining.");
+				break;
+			}
+			if(PokemonArray.size() == 0){
+				System.out.println("Congratulations you defeated all pokemons you are now crowned Trainer Supreme!!!!!");
+				break;
+			}
+		}// running the game
+	}
+
+	public static void battle(){
+		if (turn == true){
+			if(pokemonTeam.size() > 0){
+				System.out.println("\n" + user.name + " has " + user.energy + " energy");
+				int choice;
+				if(user.energy < user.leastEnergy){
+					System.out.println("You don't have enough energy for this attack");
+					choice = 2;
+				}
+				else{
+					System.out.println("What would you like to do with " + user.name + "?");
+			    	System.out.println("1. Attack      2. Pass     3. Retreat");
+			    	choice = kb.nextInt();
+				}
+				if(choice == 1){
+					System.out.println("You have " + user.nums_attack + " attacks to choose from:");
+					for (int i = 0; i < user.nums_attack; i++){
+			    		System.out.println(i+1 + ". " + user.attack.get(i).name + ", Energy: " + user.attack.get(i).energy + ", Damage: " + user.attack.get(i).damage + ", Effect: " + user.attack.get(i).effect);
+					}
+					while(true){
+						int choose = kb.nextInt() - 1;
+						if(choose + 1> 0 && choose  < user.nums_attack && user.attack.get(choose).energy < user.energy){
+							System.out.println("\n" + "-----BATTLE-----" + "\n");
+							System.out.println(user.name + " attacked " + enemy.name + " with " + user.attack.get(choice).name);
+							fight(user, enemy, choose);
+							break;
+						}
+						else{
+							System.out.println("Invalid entry please choose a valid entry");
+						}
+					}
+					turn = false;
+				}
+				if(choice == 2){
+					System.out.println("You have passed your turn");
+					turn = false;
+				}
+				if(choice == 3){
+					System.out.println(user.name + " retreat!!!!");
+					turn = false;
+				}
+			}
+		}//end turn = true
+
+		if(turn == false){
+			if(enemy.hp != 0){
+				if(enemy.energy < enemy.leastEnergy){
+					System.out.println(enemy.name + " didn't have enough energy for this round");
+					turn = true;
+				}
+				else{
+					while(true){
+						int e_attpick = (int)(Math.random()*enemy.nums_attack); // randomly choose from attacks
+						System.out.println("\n" + "-----BATTLE-----" + "\n");
+						System.out.println(enemy.name + " attacks " + user.name + " with " + enemy.attack.get(e_attpick).name + "\n");
+						fight(enemy, user, e_attpick);
+						break;
+					}
+					turn = true;
+				}
+			}
+		}//end turn = false
+
+		for(int i = 0; i < pokemonTeam.size(); i++){
+			pokemonTeam.get(i).energy += 10;
+			pokemonTeam.get(i).energy = Math.min(pokemonTeam.get(i).energy, 50); // makes sure energy doesn't go over 50
 		}
-		user_battle(user, enemy);
+		enemy.energy += 10;
+		enemy.energy = Math.min(0, 50);
+	}
+
+	public static void fight(Pokemon attacker, Pokemon attacked, int number){
+		int cost = attacker.attack.get(number).energy;
+		int damage = attacker.attack.get(number).damage;
+
+		attacked.hp -= damage;
+		attacked.hp = Math.max(0,attacked.hp);
+		attacked.energy -= cost;
+		System.out.println(attacked.name + " has " + attacked.hp + " hp left.");
+		if(attacked.hp > 0){
+			System.out.println("\n" + "---END BATTLE---" + "\n");
+		}
 	}
 }
 
